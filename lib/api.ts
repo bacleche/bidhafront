63929fcd38420478668222700a115eea8049f799
bacleche <schadracmoualou@gmail.com>
2026-06-16 10:16:28 +0100

    update du 16 juin link config with back 2

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
 // Assurez-vous que l'URL ne finit pas par un slash pour éviter les doubles //
-const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'bidhaback-production.up.railway.app/api';
+const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'https://bidhaback-production.up.railway.app/api';
 
 export const api = axios.create({
   baseURL: API_BASE,
